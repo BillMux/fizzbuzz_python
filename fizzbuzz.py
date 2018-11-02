702040fbf03a215dc97aeed2012a348d3c280e39
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-02 14:08:37 +0000

    refactor play method

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,7 @@
 class FizzBuzz:
 
     def play(num):
-        if num % 15 == 0:
-            return 'FizzBuzz'
-        if num % 3 == 0:
-            return 'Fizz'
-        if num % 5 == 0:
-            return 'Buzz'
-        else:
-            return num
+        if num % 15 == 0: return 'FizzBuzz'
+        if num % 3 == 0: return 'Fizz'
+        if num % 5 == 0: return 'Buzz'
+        return num
